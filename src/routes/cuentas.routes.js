c5a7e958145b5017952f284a2c1391b5a738523a
alvaro696 <alvaro00.am@outlook.com>
2025-03-17 16:31:42 -0400

    ultimos cambios

--- src/routes/cuentas.routes.js
+++ src/routes/cuentas.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import CuentasController from "../controllers/cuentas.controller.js";
 import { authenticationToken } from '../middlewares/authenticate.middleware.js';
+import { logAction } from "../middlewares/logAction.js";
 
 const router = Router();
 
 router.route('/').
-    get(authenticationToken, CuentasController.getCuentas).
-    post(authenticationToken, CuentasController.createCuenta);
+    get(authenticationToken, logAction, CuentasController.getCuentas).
+    post(authenticationToken, logAction, CuentasController.createCuenta);
 
 router.route('/:id')
     .put(CuentasController.updateCuenta)
--- src/routes/transacciones.routes.js
+++ src/routes/transacciones.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import TransaccionesController from "../controllers/transacciones.controller.js";
 import { authenticationToken } from '../middlewares/authenticate.middleware.js';
+import { logAction } from "../middlewares/logAction.js";
 
 const router = Router();
 
 router.route('/').
-    get(authenticationToken, TransaccionesController.obtener).
+    get(authenticationToken, logAction, TransaccionesController.obtener).
     post(TransaccionesController.crear);
 
 router.route('/:id')
